33c9bf6646d667a0be6c16f25c5b8e0c1f38aad9
Ferreira de Freitas <bofs@gft.com>
2022-11-18 19:37:57 +0100

    Initial commit

---
+++ sincronizacao-receita/src/main/java/com/example/sincronizacaoreceita/ProcessaCSV.java
@@ -0,0 +1,45 @@
+package com.example.sincronizacaoreceita;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class ProcessaCSV {
+
+    public void processarArquivo(String fileLocation) throws IOException {
+
+        FileInputStream file = new FileInputStream(new File(fileLocation));
+        Workbook workbook = new XSSFWorkbook(file);
+
+        Sheet sheet = workbook.getSheetAt(0);
+
+        Map<Integer, List<String>> data = new HashMap<>();
+        int i = 0;
+        for (Row row : sheet) {
+            data.put(i, new ArrayList<>());
+            for (Cell cell : row) {
+                switch (cell.getCellType()) {
+                    case STRING:
+                        data.get(i).add(cell.getRichStringCellValue().getString());
+                        break;
+                    case NUMERIC:
+                        if (DateUtil.isCellDateFormatted(cell)) {
+                            data.get(i).add(cell.getDateCellValue() + "");
+                        } else {
+                            data.get(i).add(cell.getNumericCellValue() + "");
+                        }
+                        break;
+                    default: data.get(i).add(" ");
+                }
+            }
+            i++;
+        }
+    }
+}
